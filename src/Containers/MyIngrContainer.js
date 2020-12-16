f7753a4b5275cc5f15f9458d66505f8a8997f5f2
liamH47 <liamhanafee@gmail.com>
2020-12-16 13:16:34 -0500

    patch done

--- src/Containers/MyIngrContainer.js
+++ src/Containers/MyIngrContainer.js
@@ -29,11 +29,32 @@ class MyIngrContainer extends Component {
             })
             .catch(console.log)
         }
+
+        stockCheck = (running_low, id) => {
+            console.log(running_low, id)
+            fetch(`http://localhost:3000/api/v1/user_ingredients/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accepts": "application/json"
+                },
+                body: JSON.stringify({ running_low })
+            })
+            .then(r => r.json())
+            .then(newIng => {
+                let copiedArray = [...this.state.userIngApi]
+                let oldIng = copiedArray.find(ing => ing.id === newIng.id)
+                let dex = copiedArray.indexOf(oldIng)
+                copiedArray[dex] = newIng
+                this.setState({ userIngApi: copiedArray })
+            })
+            .catch(console.log)
+        };
         
         
         renderMyIngredients = () => {
             let ingredientsArr = this.state.userIngApi
-            return ingredientsArr.map(ingObj => <UserIngredient deleteHandler={this.deleteHandler} key={ingObj.id} ingredient={ingObj} id={ingObj.id} category={ingObj.category} name={ingObj.name} image_url={ingObj.image_url} />)
+            return ingredientsArr.map(ingObj => <UserIngredient stockCheck={this.stockCheck} deleteHandler={this.deleteHandler} key={ingObj.id} ingredient={ingObj} id={ingObj.id} category={ingObj.category} name={ingObj.name} image_url={ingObj.image_url} />)
         }
         
         render() {
@@ -47,23 +68,6 @@ class MyIngrContainer extends Component {
 }
 
 export default MyIngrContainer
-//   runningLow = (running_low, id) => {
-//     fetch(`http://localhost:3000/api/v1/user_ingredients/${id}`, {
-//         method: 'PATCH',
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accepts": "application/json"
-//         },
-//         body: JSON.stringify ( running_low )
-//     }).then(r => r.json())
-//     .then(newIng => {
-//         let copiedArr = [...this.state.userIngApi]
-//         let index = copiedArr.findIndex(newIng => newIng.id === id)
-//         copiedArr[index] = newIng
-//         newIng.running_low = running_low
-//         this.setState({ userIngApi: copiedArr})
-//     })
-//     .catch(console.log)
-//   }
+
 
   //maybe put whole userIngredient in state and pass that as first argument, similar to in post request
