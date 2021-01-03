8e1084b24a2c421a8625159b0077cc8d4c8cfa4f
liamH47 <liamhanafee@gmail.com>
2021-01-03 18:10:36 -0500

    useringredients filtering for user id now

--- src/Components/CocktailItem.js
+++ src/Components/CocktailItem.js
@@ -8,7 +8,7 @@ class CocktailItem extends Component {
     componentDidMount() {
         this.props.fetchIngredients()
         this.props.fetchUserIngs()
-        console.log("in cocktail cdm", this.props.currentUser.user.id)
+
         // debugger
     }
 
