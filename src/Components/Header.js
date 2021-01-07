29a0ef2e1dc5c18be9322f713b9d8ac5f66bf601
liamH47 <liamhanafee@gmail.com>
2021-01-07 11:07:08 -0500

    my cocktails consolidated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         // <Sticky>
             <div className='header-grid'>
                 <Menu fluid vertical inverted>
-                    <Menu.Item as={NavLink} to="/my_cocktails">My Cocktails</Menu.Item>
+                    {/* <Menu.Item as={NavLink} to="/my_cocktails">My Cocktails</Menu.Item> */}
                     <Menu.Item as={NavLink} to="/my_ingredients">My Ingredients</Menu.Item>
                     <Menu.Item as={NavLink} to="/find_ingredients">Find Ingredients</Menu.Item>
                     <Menu.Item as={NavLink} to="/explore_cocktails">Explore Cocktails</Menu.Item>
--- src/Containers/AllCocktails.js
+++ src/Containers/AllCocktails.js
@@ -28,6 +28,11 @@ class AllCocktails extends Component {
         return searchArray.map(tailObj => <CocktailItem name={tailObj.name} image_url={tailObj.image_url} cocktail={tailObj} id={tailObj.id} key={tailObj.id} />)
      }
 
+     renderMyCocktails = () => {
+        let filtered = this.props.cocktailsApi.filter(el => this.checkCanMake(el, this.props.userIngApi) === true)
+        return filtered.map(tailObj => <CocktailItem name={tailObj.name} image_url={tailObj.image_url} cocktail={tailObj} id={tailObj.id} key={tailObj.id} />)
+     }
+
      howManyIngs = (cocktailObj) => {
         let cocktail = cocktailObj.cocktail_ingredients
         let ingNames = this.props.userIngApi.map(ingredient => ingredient.name)
@@ -47,14 +52,19 @@ class AllCocktails extends Component {
     render() {
         return (
             <> {this.props.cocktailsApi.length ? 
-                <Container>
+                <Container textAlign='center'>
                     <Segment textAlign='center' basic padded='very' vertical>
-                        <h2>All Cocktails</h2>
+                        <h2 className='content-header'>Search by Name</h2>
                         <CocktailSearch changeHandler={this.changeHandler} searchValue={this.state.searchValue} />
                     </Segment>
+                        <h2 className='content-header'>Possible Cocktails</h2>
+                        <Card.Group className='card-group' centered >
+                            {this.renderMyCocktails()}
+                        </Card.Group>
                         {/* <Item.Group relaxed divided>
                             {this.renderAllCocktails()}
                         </Item.Group> */}
+                        <h2 className='content-header'>All Cocktails</h2>
                         <Card.Group className='card-group' centered >
                             {this.renderAllCocktails()}
                         </Card.Group>
