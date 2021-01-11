a178f045de28bbecaeadb68f73ef245e161bf706
liamH47 <liamhanafee@gmail.com>
2021-01-11 15:33:25 -0500

    filter working with search, consolidating forms next

--- src/Components/CocktailFilter.js
+++ src/Components/CocktailFilter.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import { Form, Input, Dropdown } from 'semantic-ui-react'
+import React, { Component } from 'react';
 
-const CocktailFilter = ({ currentCat, categoryHandler, categoryOptions }) => {
-    return(
-        <div className='search-form'>
-            <Dropdown 
-                onChange={categoryHandler}
-                options={categoryOptions}
-                placeholder='Choose a Category'
-                selection
-                value={currentCat}
-            />
-            {/* <Form >
-                <Form.Input type="text" value={currentCat} onChange={categoryHandler} placeholder="select category" />
-            </Form> */}
-        </div>
-    ) 
-
+class CocktailFilter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
-export default CocktailFilter
+export default CocktailFilter;
