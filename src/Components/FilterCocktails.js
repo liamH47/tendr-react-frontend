aecd37ebc31ddfacef16f507f566632bbc905eea
liamH47 <liamhanafee@gmail.com>
2021-01-11 14:50:02 -0500

    filter by category working, will clean it up next

--- src/Components/FilterCocktails.js
+++ src/Components/FilterCocktails.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
-import { Dropdown, Grid, Segment, Form } from 'semantic-ui-react'
+import { Dropdown, Grid, Segment, Form, Select } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { getUserIngredients, getCocktails} from '../Redux/actions'
 
 class FilterCocktails extends Component {
 
-    state =  {
-        searchValue: "",
-        currentCat: "",
-        categoryOptions: []
-    }
+    // state =  {
+    //     searchValue: "",
+    //     currentCat: "",
+    //     categoryOptions: []
+    // }
 
-    changeHandler = (e) => {
-        this.setState({ searchValue: e.target.value })
-    }
+    // changeHandler = (e) => {
+    //     this.setState({ searchValue: e.target.value })
+    // }
 
-    categoryHandler = (e) => {
-        this.setState({...this.state, currentCat: e.target.value})
-    }
+    // categoryHandler = (e) => {
+    //     this.setState({...this.state, currentCat: e.target.value})
+    // }
 
     componentDidMount() {
         this.props.fetchCocktails()
-        console.log("in filter component cdm, props:", this.props.cocktailsApi)
-        this.setCategories()
+        // console.log("in filter component cdm, props:", this.props.cocktailsApi)
+        // this.setCategories()
     }
     
     setCategories = () => {
         let allCats = this.props.cocktailsApi.map(cocktail => cocktail.category)
         let uniqueCats = [...new Set(allCats)]
-        let selectionsObj = uniqueCats.map(category => ({ key: category, text: category, value: category }))
-        this.setState({ catOptions: selectionsObj})
+        // let selectionsObj = 
+        // return uniqueCats.map(category => ({ key: category, text: category, value: category }))
+        return uniqueCats.map(category => <option value={category}>{category}</option>)
+        // this.setState({ catOptions: selectionsObj})
         // return selectionsObj
     }
 
     render() {
         return (
         <div className='search-form'>
-            <Form >
+            {/* <Form >
                 <Form.Field>
                     <label>Search by Name</label>
                     <Form.Input type="text" value={this.state.searchValue} onChange={this.changeHandler} placeholder="search by name" />
@@ -54,7 +56,12 @@ class FilterCocktails extends Component {
                         value={this.state.currentCat}
                     />
                 </Form.Field>
-            </Form>
+            </Form> */}
+            <h1>plswork</h1>
+            <select name='category' id='category' onChange={this.props.categoryHandler}>
+                <option value="All">All</option>
+                {this.setCategories()}
+            </select>
         </div>
         );
     }
--- src/Containers/AllCocktails.js
+++ src/Containers/AllCocktails.js
@@ -12,7 +12,7 @@ class AllCocktails extends Component {
 
     state =  {
         searchValue: "",
-        currentCat: "",
+        currentCat: "All",
         categoryOptions: []
     }
 
@@ -20,18 +20,18 @@ class AllCocktails extends Component {
         this.setState({ searchValue: e.target.value })
     }
 
-    categoryHandler = (e) => {
-        this.setState({currentCat: e.target.value})
+    // change category handler
+    // conditional or ternary in render that checks if category is 'all'
+
+    // categoryHandler = (e) => {
+    //     this.setState({currentCat: e.target.value})
+    // }
+
+    categoryHandler = ({ target: { value }} ) => {
+        this.setState({ currentCat: value }, () => console.log(this.state))
     }
 
-    componentDidMount() {
-        this.props.fetchCocktails()
-        this.categoriesArr()
-        // console.log(this.props.currentUser)
-        // debugger
-    }
-
-    categoriesArr = () => {
+    setCategories = () => {
         let allCats = this.props.cocktailsApi.map(cocktail => cocktail.category)
         let uniqueCats = [...new Set(allCats)]
         let selectionsObj = uniqueCats.map(category => ({ key: category, text: category, value: category }))
@@ -39,34 +39,46 @@ class AllCocktails extends Component {
     }
 
     renderAllCocktails = () => {
-        // let filtered = this.props.cocktailsApi.filter(el => this.checkCanMake(el, this.props.userIngApi) === false)
-        // let sorted = filtered.sort((a, b) => this.howManyIngs(a) - this.howManyIngs(b))
         let sorted = this.props.cocktailsApi.sort((a, b) => this.howManyIngs(a) - this.howManyIngs(b))
-        let searchArray = sorted.filter(cocktail => cocktail.name.toLowerCase().includes(this.state.searchValue.toLowerCase()))
-        let filtered = searchArray.filter(cocktail => cocktail.category.includes(this.state.currentCat))
-        return filtered.map(tailObj => <CocktailItem name={tailObj.name} image_url={tailObj.image_url} cocktail={tailObj} id={tailObj.id} key={tailObj.id} />)
-     }
+        if(this.state.currentCat === "All"){
+            let searchArray = sorted.filter(cocktail => cocktail.name.toLowerCase().includes(this.state.searchValue.toLowerCase()))
+            // let filtered = searchArray.filter(cocktail => cocktail.category.includes(this.state.currentCat))
+            return searchArray.map(tailObj => <CocktailItem name={tailObj.name} image_url={tailObj.image_url} cocktail={tailObj} id={tailObj.id} key={tailObj.id} />)
+        } else {
+            let filtered = sorted.filter(cocktail => cocktail.category.includes(this.state.currentCat))
+            return filtered.map(tailObj => <CocktailItem name={tailObj.name} image_url={tailObj.image_url} cocktail={tailObj} id={tailObj.id} key={tailObj.id} />)
+        }
+    }
 
-     renderMyCocktails = () => {
+    componentDidMount() {
+        this.props.fetchCocktails()
+        this.setCategories()
+        // console.log(this.props.currentUser)
+        // debugger
+    }
+
+
+    
+    renderMyCocktails = () => {
         let filtered = this.props.cocktailsApi.filter(el => this.checkCanMake(el, this.props.userIngApi) === true)
         return filtered.map(tailObj => <CocktailItem name={tailObj.name} image_url={tailObj.image_url} cocktail={tailObj} id={tailObj.id} key={tailObj.id} />)
-     }
-
-     howManyIngs = (cocktailObj) => {
+    }
+    
+    howManyIngs = (cocktailObj) => {
         let cocktail = cocktailObj.cocktail_ingredients
         let ingNames = this.props.userIngApi.map(ingredient => ingredient.name)
         let hasThis = cocktail.filter((ing) => ingNames.includes(ing.name))
         return cocktail.length - hasThis.length
     }
     
-     checkCanMake(singleCockt, userIngApi) {
-         let cocktail = singleCockt.cocktail_ingredients
-         return cocktail.every(function(ing) {
-          return userIngApi.some(function(ing2) {
-             return (ing.name == ing2.name) && (ing.quantity <= ing2.quantity) 
-           })
-         })
-     }
+    checkCanMake(singleCockt, userIngApi) {
+        let cocktail = singleCockt.cocktail_ingredients
+        return cocktail.every(function(ing) {
+            return userIngApi.some(function(ing2) {
+                return (ing.name == ing2.name) && (ing.quantity <= ing2.quantity) 
+            })
+        })
+    }
     
     render() {
         return (
@@ -75,14 +87,7 @@ class AllCocktails extends Component {
                     <Segment textAlign='center' basic padded='very' vertical>
                         <h2 className='content-header'>Search by Name</h2>
                         <CocktailSearch changeHandler={this.changeHandler} searchValue={this.state.searchValue} categoryHandler={this.categoryHandler} currentCat={this.state.currentCat} categoryOptions={this.state.categoryOptions} />
-                        <Dropdown
-                            onChange={this.categoryHandler}
-                            options={this.state.categoryOptions}
-                            placeholder='Choose a Category'
-                            selection
-                            value={this.state.currentCat}
-                        />
-                        {/* <FilterCocktails categoryHandler={this.categoryHandler} currentCat={this.state.currentCat} categoryOptions={this.state.categoryOptions} /> */}
+                        <FilterCocktails categoryHandler={this.categoryHandler}/>
                     </Segment>
                         <h2 className='content-header'>All Cocktails</h2>
                         <Card.Group className='card-group' centered >
@@ -91,9 +96,6 @@ class AllCocktails extends Component {
                         <>
                         <Divider horizontal></Divider>
                         </>
-                        {/* <Item.Group relaxed divided>
-                            {this.renderAllCocktails()}
-                        </Item.Group> */}
                         <h2 className='content-header'>Possible Cocktails</h2>
                         <Card.Group className='card-group' centered >
                             {this.renderMyCocktails()}
@@ -123,3 +125,12 @@ function msp(state){
 
 
 export default connect(msp, mdp)(AllCocktails);
+{/* <Dropdown
+    onChange={this.categoryHandler}
+    options={this.state.categoryOptions}
+    placeholder='Choose a Category'
+    selection
+    value={this.state.currentCat}
+/> */}
+// let filtered = this.props.cocktailsApi.filter(el => this.checkCanMake(el, this.props.userIngApi) === false)
+// let sorted = filtered.sort((a, b) => this.howManyIngs(a) - this.howManyIngs(b))
